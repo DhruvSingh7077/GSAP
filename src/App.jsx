7a9a2ed62284a5dbc950756e73106d53319520e3
Dhruv Singh <dhruvsingh7077@gmail.com>
2025-10-17 18:54:21 +0530

    Fix Hero import casing for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 import Navbar from "./components/Navbar";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
